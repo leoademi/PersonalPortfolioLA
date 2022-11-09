621089d8311c857cf45fd6434af240e8a7d5b3b2
LeoAdemi <Admin@LEO>
2022-11-09 14:01:03 +0100

    bio krijimi

--- index.php
+++ index.php
@@ -31,3 +31,63 @@ if(isset($message)){
 }
 
 ?>
+
+<header class="header">
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+   <a href="#home" class="logo">Portfolio</a>
+
+   <nav class="navbar">
+      <a href="#home" class="active">home</a>
+      <a href="#about">about</a>
+      <a href="#services">services</a>
+      <a href="#portfolio">portfolio</a>
+      <a href="#contact">contact</a>
+   </nav>
+
+   <div class="follow">
+      <a href="#" class="fab fa-facebook-f"></a>
+      <a href="#" class="fab fa-twitter"></a>
+      <a href="#" class="fab fa-instagram"></a>
+      <a href="#" class="fab fa-linkedin"></a>
+      <a href="#" class="fab fa-github"></a>
+   </div>
+
+</header>
+
+<section class="home" id="home">
+
+   <div class="image" data-aos="fade-right">
+      <img src="images/user-image.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3 data-aos="fade-up">hi, i am leonard ademi</h3>
+      <span data-aos="fade-up">web designer & developer</span>
+      <p data-aos="fade-up">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis labore recusandae minima molestias aliquam nostrum.</p>
+      <a data-aos="fade-up" href="#about" class="btn">about me</a>
+   </div>
+
+</section>
+
+<section class="about" id="about">
+
+   <h1 class="heading" data-aos="fade-up"> <span>biography</span> </h1>
+
+   <div class="biography">
+
+      <p data-aos="fade-up">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis tenetur amet ab optio minus est error libero adipisci numquam ratione.</p>
+
+      <div class="bio">
+         <h3 data-aos="zoom-in"> <span>name : </span> Leonard Ademi  </h3>
+         <h3 data-aos="zoom-in"> <span>email : </span> leonardademi126@gmail.com </h3>
+         <h3 data-aos="zoom-in"> <span>address : </span> prishtine, kosovo </h3>
+         <h3 data-aos="zoom-in"> <span>phone : </span> +383-45-888-983 </h3>
+         <h3 data-aos="zoom-in"> <span>age : </span> 20 years </h3>
+         <h3 data-aos="zoom-in"> <span>experience : </span> 2+ years </h3>
+      </div>
+
+      <a href="#" class="btn" data-aos="fade-up">download CV</a>
+
+   </div>
